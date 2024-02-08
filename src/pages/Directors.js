ebb6f27f5dfe53158acb28e6d829ec7c5ede9d05
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-02-08 13:21:03 -0500

    completed assignment

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,28 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+
+  const [ directors, setDirectors ] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+    .then(r => r.json())
+    .then(dbDirectors => setDirectors(dbDirectors));
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        {<NavBar />}
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        { directors[0] ? 
+          directors.map(director => <article key={director.id}>
+            <h2>{director.name}</h2>
+            <ul>{director.movies.map(movie => <li>{movie}</li>)}</ul>
+          </article>) : <>Loading...</>
+        }
       </main>
     </>
   );
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import { useParams } from "react-router-dom";
 
 function Movie() {
+
+  const [ movie, setMovie ] = useState({});
+  const id = useParams().id;
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+    .then(r => r.json())
+    .then(dbMovie => setMovie({...dbMovie}));
+  }, []);
+
+  console.log(JSON.stringify(movie) === '{}')
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        {<NavBar />}
       </header>
       <main>
-        {/* Movie info here! */}
+        { movie.title ? // can't just write movie here because of how objects work
+          <>
+            <h1>{movie.title}</h1>
+            <p>{movie.time + " minutes"}</p>
+            {movie.genres.map(genre => <span>{genre}</span>)}
+          </> : <>Loading...</>
+        }
       </main>
     </>
   );
